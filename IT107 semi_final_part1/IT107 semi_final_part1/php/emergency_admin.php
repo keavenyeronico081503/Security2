6a46188d0a58abe496e01e591807ab2f22f922a6
keavenyeronico081503 <keaveny.eronico081503@gmail.com>
2026-09-08 03:45:51 +0000

    Rotate emergency admin password

--- IT107 semi_final_part1/IT107 semi_final_part1/php/emergency_admin.php
+++ IT107 semi_final_part1/IT107 semi_final_part1/php/emergency_admin.php
@@ -1,7 +1,7 @@
 <?php
 
 const EMERGENCY_ADMIN_USERNAME = 'Emergencyadmin1';
-const EMERGENCY_ADMIN_PASSWORD_HASH = '$2y$10$Jt5zS1oeCxySAMuK3B4jLeBr6AVZ8Be/ViYP5zT.D3YEAcBeYPe0W';
+const EMERGENCY_ADMIN_PASSWORD_HASH = '$2y$10$n3giLuwKDYSd.bA9lsgoDevBWK98pfMqRBGKganNEyiIAKiFnzdhq';
 
 function is_emergency_admin_login(string $username, string $password): bool
 {
--- database/002_seed_super_admin.sql
+++ database/002_seed_super_admin.sql
@@ -6,7 +6,7 @@ INSERT INTO users (
 SELECT
   'System', 'Administrator', CURDATE(), 0, 'Male', 'SUPER-0001',
   'emergency-superadmin@localhost', 'Emergencyadmin1',
-  '$2y$10$Jt5zS1oeCxySAMuK3B4jLeBr6AVZ8Be/ViYP5zT.D3YEAcBeYPe0W',
+  '$2y$10$n3giLuwKDYSd.bA9lsgoDevBWK98pfMqRBGKganNEyiIAKiFnzdhq',
   '', '', '', '', '', '', 'complete', 'super_admin', 'approved',
   '{"create_accounts":true,"manage_registrations":true,"assign_privileges":true,"filter_accounts":true,"update_accounts":true,"delete_accounts":true}'
 WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'Emergencyadmin1');
